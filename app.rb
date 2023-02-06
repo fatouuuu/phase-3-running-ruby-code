d5425a5902bcca6fbfae33ed0108a030ab02aed9
fatouuuu <fatmaabdullrahim@gmail.com>
2023-02-06 08:22:38 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -14,10 +14,29 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
+
+# START: 
+# `puts` is a built-in Ruby method that will output a string of text to the terminal (kinda like print in js)
+
+# puts "Hello world!"
+
+# print "Does the same thing"
+
+# puts [1, 2, 3].inspect
+# is the same as: 
+# p [1, 2, 3]
+# but puts [1, 2, 3] output will be: each element of the array printed to a new line on the console
+
+
+
+p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
